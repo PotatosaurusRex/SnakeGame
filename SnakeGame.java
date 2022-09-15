b4570f68b0ee4c92fdf3608a3bb9e3a6ba587c06
PotatosaurusRex <72768393+PotatosaurusRex@users.noreply.github.com>
2022-09-15 00:13:46 -0400

    Add files via upload

---
+++ SnakeGame.java
@@ -0,0 +1,13 @@
+public class SnakeGame{
+   public static void main(String[] args){
+      
+      new GameFrame();    // instead of giving name like GameFrame f1 = new GameFrame();
+   
+   
+   
+   
+   
+   
+   
+   }
+}
